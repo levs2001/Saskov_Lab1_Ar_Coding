1700c316e2f263ecec5358812efbb6c15241f1b0
levs2 <levs2@DESKTOP-LSHB5BS>
2021-11-20 15:37:45 +0300

    Java 8

--- src/main/java/MyException.java
+++ src/main/java/MyException.java
@@ -14,16 +14,36 @@ public class MyException {
 
     static public void exception(ExceptionType_e eType) {
         switch (eType) {
-            case FILE_CONFIG_NOT_OPEN -> System.out.println("Error: Can't open file with config!");
-            case FILE_READING_NOT_OPEN -> System.out.println("Error: Can't open file for reading!");
-            case FILE_WRITING_NOT_OPEN -> System.out.println("Error: Can't open file for writing!");
-            case CONFIG_UNKNOWN_PARAM -> System.out.println("Error: Unknown param, check config file!");
-            case CONFIG_NO_WORKTYPE -> System.out.println("Error: You didn't fill worktype in config!");
-            case FILE_CLOSE_ERR -> System.out.println("Error: closing files error!");
-            case CANT_WRITE_IN_CODED_FILE -> System.out.println("Error: Can't write in coded file!");
-            case CANT_WRITE_IN_DECODED_FILE -> System.out.println("Error: Can't write in file!");
-            case FILE_READING_ERR -> System.out.println("Error: Can't read byte from file");
-            case FLUSH_ERROR -> System.out.println("Error: during flushing buffer");
+            case FILE_CONFIG_NOT_OPEN:
+                System.out.println("Error: Can't open file with config!");
+                break;
+            case FILE_READING_NOT_OPEN:
+                System.out.println("Error: Can't open file for reading!");
+                break;
+            case FILE_WRITING_NOT_OPEN:
+                System.out.println("Error: Can't open file for writing!");
+                break;
+            case CONFIG_UNKNOWN_PARAM:
+                System.out.println("Error: Unknown param, check config file!");
+                break;
+            case CONFIG_NO_WORKTYPE:
+                System.out.println("Error: You didn't fill worktype in config!");
+                break;
+            case FILE_CLOSE_ERR:
+                System.out.println("Error: closing files error!");
+                break;
+            case CANT_WRITE_IN_CODED_FILE:
+                System.out.println("Error: Can't write in coded file!");
+                break;
+            case CANT_WRITE_IN_DECODED_FILE:
+                System.out.println("Error: Can't write in file!");
+                break;
+            case FILE_READING_ERR:
+                System.out.println("Error: Can't read byte from file");
+                break;
+            case FLUSH_ERROR:
+                System.out.println("Error: during flushing buffer");
+                break;
         }
     }
 }
